b03f3286644d7be1cf06adef53ad2329500bc18e
chasewilliams <chasewilliams@clearwateranalytics.com>
2020-02-25 16:13:21 -0700

    L30 Data binding on events

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -16,5 +17,9 @@ export class ServersComponent {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event) {
+    this.serverName = event.target.value;
   }
 }
