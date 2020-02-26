5acd2dda891a0c5e68302868ab15d4c0432ca95c
chasewilliams <chasewilliams@clearwateranalytics.com>
2020-02-25 17:04:46 -0700

    L32 2 way data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName = '';
+  serverName = 'TestServer';
 
   constructor() {
     setTimeout(() => {
